ad311f52a7dc697b286ab875c3e4b5c786f74cee
Albert499 <a.logozaga@alustudent.com>
2024-09-14 14:18:35 -0700

    All fixed

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""The __init__ method for models directory"""
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
+storage.reload()"""The __init__ method for models directory"""
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
+storage.reload()

15a4ced43ea21efaacabd60bfcf4f84508525cc0
Albert499 <a.logozaga@alustudent.com>
2024-09-18 05:35:51 -0700

    console.py

--- console.py
+++ console.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Defines AirBnb console."""
+"""Defines AirBnB console."""
 import cmd
-import re
-from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
@@ -13,30 +11,8 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
-
 class HBNBCommand(cmd.Cmd):
-    """Defines command interpreter.
-
-    Attributes:
-        prompt (str): The command prompt.
-    """
+    """Defines the HolbertonBnB command interpreter."""
 
     prompt = "(hbnb) "
     __all_classes = {
@@ -44,51 +20,31 @@ class HBNBCommand(cmd.Cmd):
         "User",
         "State",
         "City",
-        "Place",
         "Amenity",
+        "Place",
         "Review"
     }
 
     def emptyline(self):
-        """
-        Called when an empty line is entered.
-        If this method is not overridden like this,
-        it repeats the last nonempty
-        command entered.
-        It should not be empty_line, so ignore the typo warnings.
-        (see: https://github.com/python/cpython/blob/3.11/Lib/cmd.py)
-        """
+        """Do nothing upon receiving an empty line."""
         pass
 
-    def default(self, arg):
-        """This is the default behavior for cmd module when input is invalid"""
-        argdict = {
-            "all": self.do_all,
-            "show": self.do_show,
-            "destroy": self.do_destroy,
-            "count": self.do_count,
-            "update": self.do_update
-        }
-        match = re.search(r"\.", arg)
-        if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
-            if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
-        print("*** Unknown syntax: {}".format(arg))
-        return False
-
     def do_quit(self, arg):
-        """Quit command -> exit the program."""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """EOF signal -> exit the program."""
+        """EOF signal to exit the program."""
         print("")
         return True
+
+    def help_quit(self):
+        """Print help message for quit command."""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Print help message for EOF command."""
+        print("EOF signal to exit the program")
 
     def do_create(self, args):
         """
@@ -194,17 +150,6 @@ class HBNBCommand(cmd.Cmd):
                 object_list.append(str(all_objects[obj]))
         print(object_list)
 
-    def do_count(self, args):
-        """Usage: to count <class> or <class>.count()
-        Retrieve number of instances of given class."""
-
-        arg_list = args.split()
-        count = 0
-        for obj in storage.all().values():
-            if arg_list[0] == obj.__class__.__name__:
-                count += 1
-        print(count)
-
     def do_update(self, args):
         """Usage: to update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>) or
@@ -269,12 +214,5 @@ class HBNBCommand(cmd.Cmd):
 
         storage.save()
 
-
 if __name__ == "__main__":
-    """
-    Repeatedly issue a prompt, accept input, parse an initial prefix
-    off the received input, and dispatch to action methods, passing them
-    the remainder of the line as argument.
-    (see: https://github.com/python/cpython/blob/3.8/Lib/cmd.py#L98)
-    """
     HBNBCommand().cmdloop()
